88d383eda45213062379fdcb0e76c659e1529dc1
Naveenkumar Doddipalli <Naveenkumar.Doddipalli@fadv.com>
2018-08-28 14:49:20 +0530

    Spring security

---
+++ SpringRestSecurityOauth/src/main/java/com/naveen/services/DataService.java
@@ -0,0 +1,12 @@
+package com.naveen.services;
+
+import java.util.List;
+
+import com.naveen.models.User;
+/**
+ * @author Naveen kumar
+ *
+ */
+public interface DataService {
+	public List<User> getUserList();
+}
